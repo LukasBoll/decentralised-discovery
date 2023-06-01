86b56be8fcf4fb85a17bfcd3e4dd023d2d4a6071
LukasBoll <ga24xeg@mytum.de>
2023-06-01 20:14:05 +0200

    addGetmessages get Model

--- ProcessDiscovery/src/main/java/discovery/ProcessDiscovery/controller/CollaborationDiscoveryController.java
+++ ProcessDiscovery/src/main/java/discovery/ProcessDiscovery/controller/CollaborationDiscoveryController.java
@@ -2,10 +2,15 @@ package discovery.ProcessDiscovery.controller;
 
 import discovery.ProcessDiscovery.models.CollaborationDiscoverResponse;
 import discovery.ProcessDiscovery.models.CommunicationEvent;
+import discovery.ProcessDiscovery.models.MessageFlow;
+import discovery.ProcessDiscovery.repositories.MessageFlowRepository;
 import discovery.ProcessDiscovery.services.DecentralisedDiscoveryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 import java.util.List;
 
@@ -14,19 +19,24 @@ import java.util.List;
 public class CollaborationDiscoveryController {
 
     private final DecentralisedDiscoveryService decentralisedDiscoveryService;
+    private final MessageFlowRepository messageFlowRepository;
 
     @Autowired
-    public CollaborationDiscoveryController(DecentralisedDiscoveryService decentralisedDiscoveryService){
+    public CollaborationDiscoveryController(DecentralisedDiscoveryService decentralisedDiscoveryService, MessageFlowRepository messageFlowRepository){
         this.decentralisedDiscoveryService=decentralisedDiscoveryService;
+        this.messageFlowRepository = messageFlowRepository;
     }
 
     @GetMapping("/collaboration/discover")
-    public CollaborationDiscoverResponse discover(@RequestBody List<String> entryPoints){
-        return new CollaborationDiscoverResponse();
+    public CollaborationDiscoverResponse discover(@RequestBody List<String> entryPoints) throws ParserConfigurationException, IOException, SAXException {
+        System.out.println("Discover");
+        Document model = decentralisedDiscoveryService.getPrivateModel();
+        List<MessageFlow> messages = messageFlowRepository.findAll();
+        return new CollaborationDiscoverResponse(model, messages);
     }
 
-    @GetMapping("/collaboration/communicationevent")
-    public List<CommunicationEvent> getMessages(@RequestParam String id) throws IOException {
-        return decentralisedDiscoveryService.getCommunicationEvents(id);
+    @RequestMapping(value = "/collaboration/communicationevent/{decisionId}", method = RequestMethod.GET)
+    public List<CommunicationEvent> getMessages(@PathVariable String decisionId) throws IOException {
+        return decentralisedDiscoveryService.getCommunicationEvents(decisionId);
     }
 }
